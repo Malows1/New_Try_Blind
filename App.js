a109dd146e27b1af1412cabb9b4b26aab68d100c
Lian <markliandperez61@gmail.com>
2024-02-20 21:41:05 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { Button, View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import BlindPreview from "./blindStruc/BlindPreview"
+import LinearGradient from "react-native-linear-gradient";
+
+// Importing the BlindsGenerator module
+import { generateBlindsStructure, formatTime, incrementBlind } from "./blindStruc/BlindGenerator";
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Preview Blinds Structure"
+        onPress={() => navigation.navigate("Preview Blind Structure")}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  // Generating blinds structure using the functions from BlindsGenerator
+  const gameTime = 1800; // 30 minutes in seconds
+  const raiseBlindTime = 180; // 3 minutes in seconds
+  const blindsStructure = generateBlindsStructure(gameTime, raiseBlindTime);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen 
+          name="Preview Blind Structure"
+          options={({ navigation }) => ({
+            headerShown: true,
+            headerTitle: () => (
+              <Text style={styles.titleheader}>Preview Blind Structure</Text>
+            ),
+            headerLeft:() => (
+              <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Text style={styles.arrow}>◀︎</Text>
+              </TouchableOpacity>
+            ),
+            headerBackground: () => (
+              <LinearGradient
+                colors={["#E1BEF3", "#DA8CF8", "#7E3EF8", "#6A1FF9"]}
+                style={{ flex: 1 }}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
+              />
+            ),
+          })}
+        >
+          {(props) => <BlindPreview {...props} jsonData={blindsStructure} />}
+        </Stack.Screen>
+      </Stack.Navigator>  
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  titleheader: {
+    fontWeight:"bold",
+    marginLeft:12,
+    fontSize: 25,
+    color:"#fff",
+    left: 15,
+  },
+  arrow: {
+    fontSize: 40,
+    justifyContent: "center",
+    top: -4,
+    left: 10 
+  }
+});
+
+export default App;
---
+++ blindStruc/BlindPreview.js
@@ -0,0 +1,88 @@
+import React from "react";
+import 'react-native-gesture-handler';
+
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+
+const BlindPreview = ({ jsonData }) => {
+
+
+  return (
+    <SafeAreaView style={styles.bodybg}>
+      {/* Header row */}
+      <View style={styles.tableRow}>
+        <Text style={[styles.tableCell, styles.headerCell, styles.levelHeader]}>Level</Text>
+        <Text style={[styles.tableCell, styles.headerCell]}>Time</Text>
+        <Text style={[styles.tableCell, styles.headerCell]}>Blinds</Text>
+      </View>
+      {/* DATA ROW table*/}
+      <View style={styles.table}>
+        {jsonData.map((row, index) => (
+          <View style={styles.tableRow} key={index}>
+            {row.map((cell, cellIndex) => (
+              <Text style={[styles.tableCell, cellIndex === 0 ? styles.levelCell : null]} key={cellIndex}>
+                {cell}
+              </Text>
+            ))}
+          </View>
+        ))}
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  bodybg: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  container: {
+    height: 50,
+    justifyContent: 'center'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 23,
+    textAlign: 'center',
+  },
+  arrow: {
+    color: '#fff',
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  arrowcontainer: {
+    position: 'absolute',
+    left: 15,
+    top: -4,
+  },
+  table: {
+    marginTop: 20,
+    paddingHorizontal: 10,
+  },
+  tableRow: {
+    flexDirection: 'row',
+  },
+  tableCell: {
+    color: 'black',
+    flex: 1,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+    padding: 9,
+  },
+  headerCell: {
+    fontWeight: 'bold',
+    backgroundColor: '#f0f0f0', 
+  },
+  levelHeader: {
+    color: '#5CE3FE', // Change color of the Level header
+  },
+  levelCell: {
+    color: '#5CE3FE', // Change color of the Level column
+  },
+});
+
+export default BlindPreview;
